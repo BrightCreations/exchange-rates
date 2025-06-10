6535cd2db3c94826ed02f372478697aedffde53c
kareemshaaban221 <kareemshaaban221@gmail.com>
2025-06-10 10:24:23 +0300

    Put default values for the api service urls

--- config/exchange-rates.php
+++ config/exchange-rates.php
@@ -13,11 +13,11 @@ return [
         'exchange_rate_api' => [
             'api_key'   => env("EXCHANGE_RATE_API_TOKEN"),
             'version'   => env("EXCHANGE_RATE_API_VERSION"),
-            'base_url'  => env('EXCHANGE_RATE_API_BASE_URL'),
+            'base_url'  => env('EXCHANGE_RATE_API_BASE_URL', 'https://v6.exchangerate-api.com/v6/'),
         ],
         // @see https://docs.openexchangerates.org/reference/api-introduction
         'open_exchange_rate' => [
-            'base_url'  => env('OPEN_EXCHANGE_RATE_BASE_URL'),
+            'base_url'  => env('OPEN_EXCHANGE_RATE_BASE_URL', 'https://openexchangerates.org/api/'),
             'app_id'    => env('OPEN_EXCHANGE_RATE_APP_ID'),
         ]
     ],
